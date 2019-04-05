23a86f35717804ae170d8b6d8254b96e4e6e6827
Hicham Bezzizi <hicham_bezzizi@hotmail.com>
2019-04-05 12:25:16 +0200

    05/04

--- src/App.js
+++ src/App.js
@@ -2,8 +2,82 @@ import React from 'react';
 import './App.css';
 const moment = require('moment');
 
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      breakValue: 5,
+      sessionValue: 20,
+      time: 20 * 60 * 1000,
+      active: false,
+      mode: 'session'
+    }
+  }
+  handleSetTimers = (inc, type) => {
+    if (inc && this.state[type] === 60) return
+    if (!inc && this.state[type] === 1) return
+    this.setState({ [type]: this.state[type] + (inc ? 1 : -1) })
+  }
+  handlePlayPause = () => {
+    if (this.state.active) {
+      this.setState({ active: false }, () => clearInterval(this.pomodoro))
+    } 
+    else {
+      if (!this.state.touched) {
+        this.setState({ 
+          time: this.state.sessionValue * 60 * 1000, 
+          active: true, 
+          touched: true }, () => this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000 }) ,1000)
+        )} else {
+            this.setState({
+              active: true,
+              touched: true
+            }, () => this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000 }) ,1000))
+        }
+    }
+  }
+  handleReset = () => {
+    this.setState({ 
+      breakValue: 5, 
+      sessionValue: 20, 
+      time: 20 * 60 * 1000, 
+      active: false, 
+      mode: 'session',
+      touched: false
+    })
+    clearInterval(this.pomodoro)
+  }
+  render(){
+    return(
+    <div>
+      <Header/>
+      <div className='settings'>
+        <SetTimer 
+          type='break' 
+          label='Break Length' 
+          value={this.state.breakValue}
+          handleClick={this.handleSetTimers}
+        />
+        <SetTimer 
+          type='session' 
+          label='Session Length' 
+          value={this.state.sessionValue}
+          handleClick={this.handleSetTimers}  
+        />
+      </div>
+      <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
+      <Controls active={this.state.active} handleReset={this.handleReset} handlePlayPause={this.handlePlayPause}/>
+    </div>
+    );
+  }
+}
 
-const Header = () => <h1 className="header">Pomodoro Clock</h1>
+class Header extends React.Component {
+  render() {
+    return <h1 className="header">Pomodoro Clock</h1>
+  }
+}
+
 
 const SetTimer = ({ type, label, value, handleClick }) => (
   <div className='SetTimer'>
@@ -30,78 +104,6 @@ const Controls = ({ active, handleReset, handlePlayPause }) => (
   </div>
 )
 
-class App extends React.Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      breakValue: 5,
-      sessionValue: 25,
-      time: 25 * 60 * 1000,
-      active: false,
-      mode: 'session'
-    }
-  }
-  
-  handleSetTimers = (inc, type) => {
-    if (inc && this.state[type] === 60) return
-    if (!inc && this.state[type] === 1) return
-    this.setState({ [type]: this.state[type] + (inc ? 1 : -1) })
-  }
-  
-  handlePlayPause = () => {
-    if (this.state.active) {
-      this.setState({ active: false }, () => clearInterval(this.pomodoro))
-    } 
-    else {
-      if (!this.state.touched) {
-        this.setState({ 
-          time: this.state.sessionValue * 60 * 1000, 
-          active: true, 
-          touched: true }, () => this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000 }) ,1000)
-        )} else {
-            this.setState({
-              active: true,
-              touched: true
-            }, () => this.pomodoro = setInterval(() => this.setState({ time: this.state.time - 1000 }) ,1000))
-        }
-    }
-  }
-  
-  handleReset = () => {
-    this.setState({ 
-      breakValue: 5, 
-      sessionValue: 25, 
-      time: 25 * 60 * 1000, 
-      active: false, 
-      mode: 'session',
-      touched: false
-    })
-    clearInterval(this.pomodoro)
-  }
-  
-  render(){
-    return(
-    <div>
-      <Header/>
-      <div className='settings'>
-        <SetTimer 
-          type='break' 
-          label='Break Length' 
-          value={this.state.breakValue}
-          handleClick={this.handleSetTimers}
-        />
-        <SetTimer 
-          type='session' 
-          label='Session Length' 
-          value={this.state.sessionValue}
-          handleClick={this.handleSetTimers}  
-        />
-      </div>
-      <Timer mode={this.state.mode} time={moment(this.state.time).format('mm:ss')}/>
-      <Controls active={this.state.active} handleReset={this.handleReset} handlePlayPause={this.handlePlayPause}/>
-    </div>
-    );
-  }
-}
+
 
 export default App;
